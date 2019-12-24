a63209b891cd87e814374dac01ce9c869ece5b42
Laxmi Nag L N <laxminagln@gmail.com>
2019-12-24 17:24:56 +0530

    Create components.ino

---
+++ components.ino
@@ -0,0 +1,47 @@
+Microcontroller
+Arduino Uno (any will work)
+
+Motor Controller 
+3X L298HN - these can be gotten for free as samples from ST
+2.5" x 3.125" Perf Board
+Terminal Strips
+22AWG Solid core wire
+3X Aluminum heatsinks (I cut in half an old northbridge heatsink)
+Thermal paste
+Power
+9V Battery (to power the Arduino)
+Approximately 12V LiPo or Li-ion battery (I modified a laptop battery, so I did not even need to buy a charger)
+5V regulator (To regulate power to the motor controller logic circutry)
+9V Battery clip
+Barrel connector (Must fit the Arduino power connector)
+Other Electronics
+4X 7 RPM Gear Motor (These power two legs each)
+4X Thin linear trim pots (Rotation sensors for the legs)
+DPDT Toggle switch (Power switch)
+SPDT Slide switch (User input)
+2X Mini Snap Action Switch (Limit switch)
+3 10K resistors (Pull down)
+Headers
+Signal Wire (Old IDE cables work really well, and let you organize your wires easily)
+Heat Shrink Tubing
+Hardware
+12'  3/4" x 1/8" Aluminum Bar (These come in 6' lengths at my local hardware store)
+6" x 3" acrylic sheet (Electronics are mounted to this)
+6x Standoffs with screws
+1' Threaded rod and corresponding 1/2" nut
+2X 1' x 3/16" steel rod
+1' x 3/16" I.D. Brass Tubing
+4X 5mm Aluminum Universal Mounting Hub
+Pack of large T Pins
+4X 3/32 screws (to mount the motors)
+An assortment of 4/40 screws and nuts
+Assorted hex screws and nuts
+4X Bic pens (I used the plastic shafts to fix the pots on the legs in place)
+4X Locknuts
+5 Minute epoxy
+Sheet metal scraps (For spacing and mounting things. Bits of Meccano work well)
+Stick on Velcro (For holding on the batteries)
+Hard Drive reading head bearing
+3/4" Plastic angle
+Electrical Tape
+Zip Ties
